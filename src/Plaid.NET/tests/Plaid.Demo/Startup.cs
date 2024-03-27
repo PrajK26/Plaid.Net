c9f35713539c9e85bba71ec2557b3b4c58a6cd00
prajakta <khatriprajakta26@gmail.com>
2024-03-27 12:43:56 +0530

    2703 - intial commit

---
+++ src/Plaid.NET/tests/Plaid.Demo/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Acklann.Plaid.Demo.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Acklann.Plaid.Demo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<PlaidCredentials>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseNodeModules(env.ContentRootPath);
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute("default", "{Controller=Home}/{Action=Index}");
+            });
+        }
+    }
+}
